eee301985dd4bde6214477d541698af837e855b6
root <root@Unaislaptop>
2023-09-26 19:20:27 +0530

    setup routes for all ts file inside routes folder

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import { json } from 'body-parser';
 import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signupRouter } from './routes/signup';
+import { signoutRouter } from './routes/signout';
 
 const app = express();
 
 app.use(json());
 
 app.use(currentUserRouter)
+app.use(signinRouter)
+app.use(signupRouter)
+app.use(signoutRouter)
 
 app.listen(3000,()=>{
     console.log('auth listening on port 3000');
