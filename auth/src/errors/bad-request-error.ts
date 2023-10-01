e4fea49ae89052745241588813ac9caf85bbcf5b
root <root@Unaislaptop>
2023-10-01 17:41:44 +0530

    just added the feature of hashing password when user signup

--- auth/src/errors/bad-request-error.ts
+++ auth/src/errors/bad-request-error.ts
@@ -3,7 +3,7 @@ import { CustomError } from "./custom-error";
 export class BadRequestError extends CustomError{
     statusCode = 400;
 
-    constructor(message:string){
+    constructor(public message:string){
         super(message)
 
         Object.setPrototypeOf(this,BadRequestError.prototype)
--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,43 +1,55 @@
-import mongoose from 'mongoose';
+    import mongoose from 'mongoose';
+    import { Password } from '../services/password';
 
-//Interface describes the properties that are required to create user
-interface UserAttr{
-    email:string,
-    password:string
-}
-
-//Interface describes the properties that requires that a User model has
-interface UserModel extends mongoose.Model<UserDoc>{
-    build(attr:UserAttr):UserDoc;
-}
-
-//Intrface describes the properties that a User document has
-
-interface UserDoc extends mongoose.Document{
-    email:string,
-    password:string,
-    
-}
-
-const userSchema = new mongoose.Schema({
-    email :{
-        type : String,
-        required : true
-    },
-    password:{
-        type: String,
-        reqired : true
+    //Interface describes the properties that are required to create user
+    interface UserAttr{
+        email:string,
+        password:string
     }
-})
 
-userSchema.statics.build= (attr:UserAttr)=>{
-    return new User(attr)
-}
+    //Interface describes the properties that requires that a User model has
+    interface UserModel extends mongoose.Model<UserDoc>{
+        build(attr:UserAttr):UserDoc;
+    }
 
-const User  = mongoose.model<UserDoc,UserModel>('User', userSchema);
+    //Intrface describes the properties that a User document has
+
+    interface UserDoc extends mongoose.Document{
+        email:string,
+        password:string,
+        
+    }
+
+    const userSchema = new mongoose.Schema({
+        email :{
+            type : String,
+            required : true
+        },
+        password:{
+            type: String,
+            reqired : true
+        }
+    })
+
+    userSchema.pre('save', async function(done){
+        if(this.isModified('password')){
+            const password = this.get('password');
+            if (password) {
+                const hashed = await Password.toHash(password);
+                this.set('password', hashed);
+            }
+        }
+        done();
+    });
+
+    userSchema.statics.build= (attr:UserAttr)=>{
+        return new User(attr)
+    }
+
+    const User  = mongoose.model<UserDoc,UserModel>('User', userSchema);
 
 
 
 
 
-export { User }
+    export { User }
