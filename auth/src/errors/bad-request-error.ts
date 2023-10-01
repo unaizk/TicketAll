3fc67beba14f45ecb3f844f05f4df290f615fe8c
root <root@Unaislaptop>
2023-10-01 14:22:25 +0530

    created a global error to handle a existing email user

---
+++ auth/src/errors/bad-request-error.ts
@@ -0,0 +1,19 @@
+import { CustomError } from "./custom-error";
+
+export class BadRequestError extends CustomError{
+    statusCode = 400;
+
+    constructor(message:string){
+        super(message)
+
+        Object.setPrototypeOf(this,BadRequestError.prototype)
+    }
+
+    serialize(){
+       return [{
+            message:this.message
+        }]
+    }
+
+
+}
