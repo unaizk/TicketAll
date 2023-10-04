4e5abd173fc79c30946a6a41ae05dac8e076e9d8
root <root@Unaislaptop>
2023-10-04 14:05:14 +0530

    just tested all the test cases inside signup.ts file

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -3,7 +3,6 @@ import request from "supertest";
 
 it('Return a 201 on succesfull signup',async()=>{
    
-   
     return request(app)
         .post('/api/users/signup')
         .send({
@@ -12,4 +11,73 @@ it('Return a 201 on succesfull signup',async()=>{
         })
 
         .expect(201);
+})
+
+
+it('Return a 400 on Invalid email',async()=>{
+   
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email:'unaiskgmail.com',
+            password:"1234"
+        })
+
+        .expect(400);
+})
+
+it('Return a 400 on Invalid password',async()=>{
+   
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email:'unaisk@gmail.com',
+            password:"1"
+        })
+
+        .expect(400);
+})
+
+
+it('Return a 400 on empty password and email',async()=>{
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+        
+            password:"1234"
+        })
+
+        .expect(400);
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email:'unaisk@gmail.com',
+      
+    })
+
+    .expect(400);
+})
+
+
+it('Email already exisiting error',async()=>{
+
+    await request(app)
+        .post('/api/users/signup')
+        .send({
+            email:'unaisk@gmail.com',
+            password:"1234"
+        })
+
+        .expect(201);
+
+    await request(app)
+    .post('/api/users/signup')
+    .send({
+        email:'unaisk@gmail.com',
+        password:"1234"
+    })
+
+    .expect(400);
 })
